391a1ca677a5826ff1e475b4419f8b27e983e876
Ebraheem Haneef <EbraheemHaneef@gmail.com>
2016-11-15 11:31:26 -0600

    added pickels

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-something = () => {
-  console.log("Hello World!")
+function pickels(){
+    var pickel1 = "hello";
+    var pickel2 = "I'm a pickel";
+    var pickle3 = "Hi my name is bob";
+    console.log (pickel1, pickel2, pickel3);
 }
-
-something()
